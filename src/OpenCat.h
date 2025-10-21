e325af399f09914aeb199eee1208d41feb013f11
Rongzhong Li <rzlib2l@gmail.com>
2025-10-21 12:01:13 +0800

    rename petoi web coding blocks

--- src/OpenCat.h
+++ src/OpenCat.h
@@ -89,7 +89,7 @@ String uniqueName = "";
 #define BT_BLE  // toggle Bluetooth Low Energy (BLE）
 #define BT_SSP  // toggle Bluetooth Secure Simple Pairing (BT_SSP)
 // #define WIFI_MANAGER  // toggle WiFi Manager. It should be always off for now
-// #define WEB_SERVER // toggle web server
+#define WEB_SERVER // toggle web server
 // #define SHOW_FPS // toggle FPS display
 #ifndef VT
 #define GYRO_PIN  // toggle the Inertia Measurement Unit (IMU), i.e. the gyroscope
